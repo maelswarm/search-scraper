6655d6c5bb04de8e8c96821b1019679337f2acec
Joe Grotto <joegrotto@heir.company>
2017-12-10 16:31:53 -0600

    async for loop with page stats and working usability

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const cheerio = require('cheerio');
 const argv = require('minimist')(process.argv.slice(2), {});
 
 const options = {
-  url:
-    'https://kgsearch.googleapis.com/v1/entities:search?query=' +
+  url: 'https://kgsearch.googleapis.com/v1/entities:search?query=' +
     argv._[0] +
     '&key=AIzaSyAaBLhs1pKLTQyGD8CT0N4CSeqM2q-6G7s&limit=1&indent=True'
 };
 
 const psoptions = {
-  url:
-    'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=' +
+  url: 'https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=' +
     argv._[0] +
     '&strategy=mobile&key=AIzaSyAaBLhs1pKLTQyGD8CT0N4CSeqM2q-6G7s'
 };
@@ -28,20 +26,20 @@ if (argv.t) {
   options.url += '&types=' + argv.t;
 }
 
-request(options, function(error, response, html) {
+request(options, function (error, response, html) {
   if (!error) {
     const $ = cheerio.load(html);
     console.log(html);
     if (JSON.parse(html).itemListElement[0] !== undefined) {
       if (
         JSON.parse(html).itemListElement[0].result.detailedDescription
-          .articleBody !== undefined
+        .articleBody !== undefined
       ) {
         console.log(
           '\n' +
-            JSON.parse(html).itemListElement[0].result.detailedDescription
-              .articleBody +
-            '\n'
+          JSON.parse(html).itemListElement[0].result.detailedDescription
+          .articleBody +
+          '\n'
         );
       }
     }
@@ -49,27 +47,27 @@ request(options, function(error, response, html) {
 });
 
 if (argv._[0].indexOf('http://') > -1 || argv._[0].indexOf('https://') > -1) {
-  request(psoptions, function(error, response, html) {
+  request(psoptions, function (error, response, html) {
     if (!error) {
       //console.log(html);
       const json = JSON.parse(html);
       console.log(
         '\nSpeed: ' +
-          json.ruleGroups.SPEED.score +
-          '/100\nUsability:' +
-          json.ruleGroups.USABILITY.score +
-          '/100\n'
+        json.ruleGroups.SPEED.score +
+        '/100\nUsability:' +
+        json.ruleGroups.USABILITY.score +
+        '/100\n'
       );
     }
   });
 }
 
-request(wikioptions, function(error, response, html) {
+request(wikioptions, function (error, response, html) {
   const $ = cheerio.load(html);
   console.log(
     $('.mw-parser-output p')
-      .eq(0)
-      .text()
+    .eq(0)
+    .text()
   );
 });
 
@@ -80,72 +78,57 @@ request(googleOptions, (error, response, html) => {
 
   let results = [];
 
-  $('cite').each(function(i, element) {
+  $('cite').each(function (i, element) {
     if (
       $(this)
-        .text()
-        .indexOf('http://') > -1 ||
+      .text()
+      .indexOf('http://') > -1 ||
       $(this)
-        .text()
-        .indexOf('https://') > -1
+      .text()
+      .indexOf('https://') > -1
     ) {
       results[i] = $(this).text();
     } else if (
       $(this)
-        .text()
-        .indexOf('www') > -1
+      .text()
+      .indexOf('www') > -1
     ) {
       results[i] = `http://${$(this).text()}`;
-    } else if (
-      $(this)
-        .text()
-        .indexOf(' ') > -1
-    ) {
+    } else {
       results[i] = `http://${$(this)
         .text()
         .replace(/\s+/g, '')
         .toLowerCase()}.com/`;
-    } else {
-      results[i] = `http://${$(this)
-        .text()
-        .toLowerCase()}/`;
     }
   });
 
   results.join(' ');
   console.log('Google Search Rankings:\n', results);
 
-  function gLoop(i) {
-    if (i < results.length) {
-      const PS_API = {
-        url: `https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=${
-          results[i]
-        }&key=AIzaSyAaBLhs1pKLTQyGD8CT0N4CSeqM2q-6G7s`
-      };
+  for (let result of results) {
+    const PS_API = {
+      url: `https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=${
+        result
+      }&key=AIzaSyAaBLhs1pKLTQyGD8CT0N4CSeqM2q-6G7s&strategy=mobile`
+    };
 
-      request(PS_API, function(error, response, html) {
-        if (!error) {
-          const json = JSON.parse(html);
-          console.log(json);
-          console.log(results[i]);
 
-          if (json.ruleGroups !== undefined) {
-            if (json.ruleGroups.SPEED !== undefined) {
-              console.log(`Speed: ${json.ruleGroups.SPEED.score}`);
-            }
+    request(PS_API, async function (error, response, html) {
+      // if (error) {
+      //   console.log(error);
+      // }
+      if (!error) {
 
-            if (json.ruleGroups.USABILITY !== undefined) {
-              console.log(`Usability: ${json.ruleGroups.USABILITY.score}`);
-            }
-          }
+        const json = await JSON.parse(html);
+        console.log(json);
+        console.log(
+          `${result}:\nSpeed: ${
+              json.ruleGroups.SPEED.score
+            }/100\nUsability: ${json.ruleGroups.USABILITY.score
+            }/100\nPage Stats: ${JSON.stringify(json.pageStats)}\n`
+        );
+      }
+    });
 
-          console.log('');
-          i++;
-          gLoop(i + 1);
-        }
-      });
-    }
   }
-
-  gLoop(0);
 });

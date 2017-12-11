07b1f00d820fe25d954cd9c5cca22962bec97dc4
Joe Grotto <joegrotto@heir.company>
2017-12-10 20:06:30 -0600

    seperate functions for pagespeed and metadata

--- index.js
+++ index.js
@@ -71,6 +71,64 @@ request(wikioptions, function (error, response, html) {
   );
 });
 
+const pageSpeedAPI = (url) => {
+  const PS_API = {
+    url: `https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=${
+        url
+      }&key=AIzaSyAaBLhs1pKLTQyGD8CT0N4CSeqM2q-6G7s&strategy=mobile`
+  };
+
+  request(PS_API, async function (error, response, html) {
+    if (error) {
+      console.log(error);
+    }
+    if (!error) {
+      const json = await JSON.parse(html);
+      // console.log(json);
+
+      const ps = `PAGE SPEED:
+      \nTitle: ${json.title}
+      \nURL: ${json.id}
+      \nSpeed: ${json.ruleGroups.SPEED.score}
+      \nUsability: ${json.ruleGroups.USABILITY.score}
+      \n
+      `
+
+      console.log(ps);
+    }
+  });
+}
+
+const getMetadata = (url) => {
+  request(url, async function (error, response, html) {
+    if (error) {
+      console.log(error);
+    }
+
+    if (!error) {
+      const $ = cheerio.load(html);
+
+      const meta =
+        `METADATA:
+          \n${url}
+          \nTitle: ${$('meta[name="title"]').attr('content')}
+          \nKeywords: ${$('meta[name="keywords"]').attr('content')}
+          \nDescription: ${$('meta[name="description"]').attr('content')}
+          \n`
+
+      // {
+      //   title: $('meta[name="title"]').attr('content'),
+      //   keywords: $('meta[name="keywords"]').attr('content'),
+      //   description: $('meta[name="description"]').attr('content')
+      // }
+
+      return meta;
+
+      console.log(meta);
+    }
+  });
+}
+
 request(googleOptions, (error, response, html) => {
   const $ = cheerio.load(html);
   //   console.log($('#ires'));
@@ -78,7 +136,7 @@ request(googleOptions, (error, response, html) => {
 
   let results = [];
 
-  $('cite').each(function (i, element) {
+  $('.s cite').each(function (i, element) {
     if (
       $(this)
       .text()
@@ -88,17 +146,8 @@ request(googleOptions, (error, response, html) => {
       .indexOf('https://') > -1
     ) {
       results[i] = $(this).text();
-    } else if (
-      $(this)
-      .text()
-      .indexOf('www') > -1
-    ) {
-      results[i] = `http://${$(this).text()}`;
     } else {
-      results[i] = `http://${$(this)
-        .text()
-        .replace(/\s+/g, '')
-        .toLowerCase()}.com/`;
+      results[i] = `http://${$(this).text()}`;
     }
   });
 
@@ -106,29 +155,9 @@ request(googleOptions, (error, response, html) => {
   console.log('Google Search Rankings:\n', results);
 
   for (let result of results) {
-    const PS_API = {
-      url: `https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=${
-        result
-      }&key=AIzaSyAaBLhs1pKLTQyGD8CT0N4CSeqM2q-6G7s&strategy=mobile`
-    };
+    pageSpeedAPI(result);
 
-
-    request(PS_API, async function (error, response, html) {
-      // if (error) {
-      //   console.log(error);
-      // }
-      if (!error) {
-
-        const json = await JSON.parse(html);
-        console.log(json);
-        console.log(
-          `${result}:\nSpeed: ${
-              json.ruleGroups.SPEED.score
-            }/100\nUsability: ${json.ruleGroups.USABILITY.score
-            }/100\nPage Stats: ${JSON.stringify(json.pageStats)}\n`
-        );
-      }
-    });
-
+    getMetadata(result);
   }
+
 });
